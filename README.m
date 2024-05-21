8cb6a9d25c8a07e98eb1d6e232143d4cfa03fd36
victorH200 <0000110524989xsp@al.educacao.sp.gov.br>
2024-05-21 09:45:15 -0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,14 @@
+### bem vindo ao meu perfil
+
+meu nome è victor
+
+estou estudando no alura
+estou me desenvolvendo na linguagem java script
+utilizo esse espaco para minha organizacao e conpartilhamento dos meus projetos desenvouvidos
+
+### voce pode entrar em contato comigo aqui
+
+0000110524989xsp@al.educacao.sp.gov.br
+
+
+![](https://media1.tenor.com/m/SfIBJjbHH9UAAAAd/dog-smile-shyboos.gif)
